15e47f521a8f92fdc434d75c693ef922e97c0c8c
gayeonHong <hong52321579@gmail.com>
2025-11-01 15:32:09 +0900

    update vite config

--- CookUs/vite.config.ts
+++ CookUs/vite.config.ts
@@ -6,21 +6,11 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
   const target = env.VITE_API_URL || 'http://localhost:8600'
 
-  console.log(target)
-
   return {
     plugins: [react()],
     server: {
       proxy: {
-        // '/auth/.*': { target, changeOrigin: true },
-        '/auth/login': { target, changeOrigin: true },
-        '/auth/logout': { target, changeOrigin: true },
-        '/auth/signup': { target, changeOrigin: true },
-        '/auth/refresh': { target, changeOrigin: true },
-        '/auth/find-id': { target, changeOrigin: true },
-        '/auth/find-id/verify': { target, changeOrigin: true },
-        '/auth/find-password': { target, changeOrigin: true },
-        '/auth/password-set': { target, changeOrigin: true },
+        '/auth': { target, changeOrigin: true },
         '/me': { target, changeOrigin: true },
         '/recipes': { target, changeOrigin: true },
         '/recommendations': { target, changeOrigin: true },
